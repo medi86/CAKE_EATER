1e05a16c13dd6e84b40236b72793eb46401afefb
Josh Cheek <josh.cheek@gmail.com>
2016-02-01 00:26:56 -0600

    Talk to the websocket (though not sending anything interesting right now)

--- serve.rb
+++ serve.rb
@@ -13,6 +13,7 @@ users = [
 cake_eater = NetworkGames::CakeEaterApp.new users: users, registration_time: 5*60
 
 Thread.new do
+  Thread.current.abort_on_exception = true
   loop do
     sleep 1
     cake_eater.timer.check
@@ -24,38 +25,46 @@ require 'celluloid/autostart'
 require 'reel'
 require 'rack'
 
+observers = []
+at_exit { observers.each &:close }
+update_websockets = lambda do
+  cake_eater.timer.register :update_websockets, 1, &update_websockets
+  observers.reject!(&:closed?)
+  observers.each do |websocket|
+    websocket << JSON.dump(cake_eater.game.look('team1'))
+  end
+end
+update_websockets.call
+
 Reel::Server::HTTP.supervise(host, port) do |connection|
   connection.each_request do |request|
     if request.websocket?
-      # var ws=new WebSocket('ws://localhost:3000'); ws.onmessage = function(arg) { console.log(arg.data) };
-      puts "Client made a WebSocket request to: #{request.url}"
-      websocket = request.websocket
-      websocket << "Hello everyone out there in WebSocket land"
-      websocket.close
-    else
-      # Mostly stolen from https://github.com/celluloid/reel-rack/blob/2edd5ff371a94eca79791d5312aae8065b42b714/lib/reel/rack/server.rb#L71
-      options = { :method => request.method, :input => request.body.to_s, "REMOTE_ADDR" => request.remote_addr }
-      request.headers.each { |key, value|
-        header = key.upcase.gsub('-','_')
-        header = "HTTP_#{header}" unless header == 'CONTENT_TYPE' || header == 'CONTENT_LENGTH'
-        options[header] = value
-      }
-
-      # This bit I made up :P
-      env      = Rack::MockRequest.env_for(request.url, options)
-      filepath = File.join(public_dir, env['PATH_INFO'])
-      if File.exist? filepath
-        body    = File.read(filepath)
-        ext     = File.extname(filepath)
-        type    = {'.js' => 'text/javascript', '.html' => 'text/html'}.fetch(ext)
-        headers = {'Content-Length' => body.bytesize, 'Content-Type' => "#{type}; charset=UTF-8"}
-        status  = 200
-      else
-        status, headers, body = cake_eater.call(env)
-        body = body.join("\n")
-      end
-      request.respond status, headers, body
+      observers << request.websocket
+      next
     end
+
+    # Mostly stolen from https://github.com/celluloid/reel-rack/blob/2edd5ff371a94eca79791d5312aae8065b42b714/lib/reel/rack/server.rb#L71
+    options = { :method => request.method, :input => request.body.to_s, "REMOTE_ADDR" => request.remote_addr }
+    request.headers.each { |key, value|
+      header = key.upcase.gsub('-','_')
+      header = "HTTP_#{header}" unless header == 'CONTENT_TYPE' || header == 'CONTENT_LENGTH'
+      options[header] = value
+    }
+
+    # This bit I made up :P
+    env      = Rack::MockRequest.env_for(request.url, options)
+    filepath = File.join(public_dir, env['PATH_INFO'])
+    if File.exist? filepath
+      body    = File.read(filepath)
+      ext     = File.extname(filepath)
+      type    = {'.js' => 'text/javascript', '.html' => 'text/html'}.fetch(ext)
+      headers = {'Content-Length' => body.bytesize, 'Content-Type' => "#{type}; charset=UTF-8"}
+      status  = 200
+    else
+      status, headers, body = cake_eater.call(env)
+      body = body.join("\n")
+    end
+    request.respond status, headers, body
   end
 end
 
